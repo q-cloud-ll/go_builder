9cd9a0acc6991e044c010194de42d2f99bd6cde0
cherry <cherryqcsj@gmail.com>
2023-07-09 15:57:02 +0800

    fix(server) windos not build initServer

--- setting/server/server.go
+++ setting/server/server.go
@@ -7,7 +7,6 @@ import (
 
 	"go.uber.org/zap"
 
-	"github.com/fvbock/endless"
 	"github.com/gin-gonic/gin"
 
 	"time"
@@ -15,14 +14,6 @@ import (
 
 type server interface {
 	ListenAndServe() error
-}
-
-func initServer(address string, router *gin.Engine) server {
-	s := endless.NewServer(address, router)
-	s.ReadHeaderTimeout = 20 * time.Second
-	s.WriteTimeout = 20 * time.Second
-	s.MaxHeaderBytes = 1 << 20
-	return s
 }
 
 func RunWindowServer(router *gin.Engine) {
