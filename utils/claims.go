c39f01e0876226c83638103d8b11eede395f5796
cherry <cherryqcsj@gmail.com>
2023-07-09 14:12:01 +0800

    add utils get userid from jwt

---
+++ utils/claims.go
@@ -0,0 +1,46 @@
+package utils
+
+import (
+	"project/global"
+	systemReq "project/model/request"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetClaims(c *gin.Context) (*systemReq.CustomClaims, error) {
+	token := c.Request.Header.Get("x-token")
+	j := NewJWT()
+	claims, err := j.ParseToken(token)
+	if err != nil {
+		global.GB_LOG.Error("从Gin的Context中获取从jwt解析信息失败, 请检查请求头是否存在x-token且claims是否为规定结构")
+	}
+	return claims, err
+}
+
+// GetUserUId 从Gin的Context中获取从jwt解析出来的用户UID
+func GetUserUId(c *gin.Context) int64 {
+	if claims, exists := c.Get("claims"); !exists {
+		if cl, err := GetClaims(c); err != nil {
+			return 0
+		} else {
+			return cl.UID
+		}
+	} else {
+		waitUse := claims.(*systemReq.CustomClaims)
+		return waitUse.UID
+	}
+}
+
+// GetUserID 从Gin的Context中获取从jwt解析出来的用户ID
+func GetUserID(c *gin.Context) uint {
+	if claims, exists := c.Get("claims"); !exists {
+		if cl, err := GetClaims(c); err != nil {
+			return 0
+		} else {
+			return cl.BaseClaims.ID
+		}
+	} else {
+		waitUse := claims.(*systemReq.CustomClaims)
+		return waitUse.BaseClaims.ID
+	}
+}
